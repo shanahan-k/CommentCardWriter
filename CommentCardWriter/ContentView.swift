00aa75c5169003f47ca830fe2d124f9c2bd8c2ee
Krystian Shanahan <shanahan.k@etoncollege.org.uk>
2022-02-08 11:18:49 +0000

    first MVP

--- CommentCardWriter/ContentView.swift
+++ CommentCardWriter/ContentView.swift
@@ -8,9 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var subject : String = ""
+    @State private var classworkFeeling : String =  ""
+    @State private var EWFeeling : String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Form{
+                TextField("What subject?", text: $subject)
+
+            }
+            Form {
+                Text("How are you Feeling (Classwork)?")
+                HStack {
+                    Button("😃") {
+                        classworkFeeling = "Good"
+                    }
+                    Spacer()
+                    Button("🙂") {
+                        classworkFeeling = "OK"
+                    }
+                    Spacer()
+                    Button("🙁") {
+                        classworkFeeling = "not so good"
+                    }
+                
+                }
+            }
+            Spacer()
+            Form {
+                Text("How are you Feeling (EW)?")
+                HStack {
+                    Button("😃") {
+                        EWFeeling = "Good"
+                    }
+                    Spacer()
+                    Button("🙂") {
+                        EWFeeling = "OK"
+                    }
+                    Spacer()
+                    Button("🙁") {
+                        EWFeeling = "Bad"
+                    }
+                
+                }
+                
+                
+                
+            }
+            Spacer()
+            Form {
+            Text("I am finding \(subject) \(classworkFeeling) in class and I am finding the EW \(EWFeeling).")
+            }
+            Spacer()
+
+            
+        }
     }
 }
 
