c28bdfeeef63e82bb53b079751b9040afb02d5a9
Krystian Shanahan <shanahan.k@etoncollege.org.uk>
2022-02-08 10:51:25 +0000

    Initial Commit

---
+++ CommentCardWriter/CommentCardWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardWriterApp.swift
+//  CommentCardWriter
+//
+//  Created by Shanahan, Krystian (JD) on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommentCardWriter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CommentCardWriter
+//
+//  Created by Shanahan, Krystian (JD) on 08/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
